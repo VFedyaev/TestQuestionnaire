d38d2084d35a1c0bbd9aeeb403bde6133544f540
VFedyaev <31606465+VFedyaev@users.noreply.github.com>
2019-06-06 14:18:20 +0600

    add mapping, platformInitializer

--- Questionnaire/Questionnaire.Common/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Questionnaire.Common
-{
-    public class Class1
-    {
-    }
-}
---
+++ Questionnaire/Questionnaire.Common/PlatformInitializer.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Questionnaire.Common.Interfaces;
+using Questionnaire.Data.Interfaces;
+using Questionnaire.Service.Interfaces;
+using Questionnaire.Data.Patterns;
+using Questionnaire.Service.Services;
+
+namespace Questionnaire.Common
+{
+    public class PlatformInitializer : IPlatformInitializer
+    {
+        protected IConfiguration _configuration;
+        public PlatformInitializer(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Configure DataBase
+            ConfigureDatabase(services);
+
+            // Add singletone instances
+            services.AddSingleton(Mapping.Configuration.CreateDefaultMapper());
+
+            // Add scoped registrations
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IAnswerService, AnswerService>();
+        }
+
+        protected virtual void ConfigureDatabase(IServiceCollection services)
+        {
+            services.AddDbContext<DbContext, Data.QuestionnaireContext>(options => options.UseSqlServer(_configuration.GetConnectionString("DefaultConnection")));
+        }
+    }
+}
