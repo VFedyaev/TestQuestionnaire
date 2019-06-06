f6d8fbdbb04562540bff1ae56ad5cef8155e3c9f
VFedyaev <31606465+VFedyaev@users.noreply.github.com>
2019-06-06 15:00:21 +0600

    add controllers

---
+++ Questionnaire/Questionnaire.WEB/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Questionnaire.Service.Interfaces;
+using Questionnaire.WEB.Models;
+
+namespace Questionnaire.WEB.Controllers
+{
+    public class HomeController : BaseController
+    {
+        public HomeController(IMapper mapper, IAnswerService answerService, UserManager<User> userManager) : base(mapper, answerService, userManager) { }
+
+        public IActionResult Index(string query)
+        {
+            ViewBag.SearchQuery = (query ?? "");
+
+            return View();
+        }
+
+        public JsonResult GetData(int pageNumber, string query, int sort)
+        {
+            var pageSize = 20;
+            var models =  GetData(pageSize, pageNumber, sort, query);
+
+            return Json(new { models.Data, total = models.RecordsTotal, filtered = models.RecordsFiltered });
+        }
+
+        [Authorize]
+        public IActionResult Answer()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> Answer(AnswerViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var currentUser = await _userManager.GetUserAsync(HttpContext.User);
+                model.Answer.UserId = Guid.Parse(currentUser.Id);
+
+                _answerService.AddAnswer(model.Answer);
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        [Authorize]
+        public IActionResult About(string answerId)
+        {
+            var answer = _answerService.GetAnswerById(Guid.Parse(answerId));
+            var viewModel = new AnswerViewModel
+            {
+                Answer = answer
+            };
+            return View(viewModel);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
--- Questionnaire/Questionnaire.WEB/Startup.cs
+++ Questionnaire/Questionnaire.WEB/Startup.cs
@@ -1,16 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using AutoMapper.Configuration;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Questionnaire.Common;
+using Questionnaire.Data;
+using Questionnaire.WEB.Data;
+using Questionnaire.WEB.Models;
 
 namespace Questionnaire.WEB
 {
